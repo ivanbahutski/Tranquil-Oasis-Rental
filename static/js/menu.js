2c593f234f51f6780d2b5b4184977e5bd77fbbcf
Ivan Bahuski <ibahutski@gmail.com@gmail.com>
2025-05-01 21:04:29 +0200

    Settings

--- static/js/menu.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('open');
-});
